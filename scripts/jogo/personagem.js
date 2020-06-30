eb5d0c534d20c80d2b9f3dc6e32661c2ba5c4e7d
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-06-30 18:12:28 -0300

    Finished third class content

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,16 +1,23 @@
 class Personagem extends Animacao{
-  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
-    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite){
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
     
-    this.yInicial = height - this.altura;
+    this.variacaoY = variacaoY;
+    this.yInicial = height - this.altura - this.variacaoY;
     this.y = this.yInicial;
     
     this.velocidadeDoPulo = 0;
-    this.gravidade = 3;
+    this.gravidade = 6;
+    this.alturaDoPulo = -50;
+    this.pulos = 0;
   }
   
   pula() {
-    this.velocidadeDoPulo = -30
+    if(this.pulos < 2) {
+      this.velocidadeDoPulo = this.alturaDoPulo;
+      somDoPulo.play()
+      this.pulos++;
+    }
   }
   
   aplicaGravidade() {
@@ -18,12 +25,13 @@ class Personagem extends Animacao{
     this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
     
     if(this.y > this.yInicial){
-      this.y = this.yInicial
+      this.y = this.yInicial;
+      this.pulos = 0;
     }
   }
   
   estaColidindo(inimigo) {
-    const precisao = .7
+    const precisao = 0.7
     const colisao = collideRectRect(
       this.x, 
       this.y, 
